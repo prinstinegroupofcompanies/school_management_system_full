d8bf18a11c2840f6ea9f3a7fd245538a36a879ea
Samson Bryant <samsonbryant89@gmail.com>
2025-10-24 23:51:51 +0000

    Add frontend asset building: TailwindCSS configuration and build scripts

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+    "./app/**/*.php",
+    "./public/**/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
